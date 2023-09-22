2134fe526bfadb0dbdffb8c263bcf22bb611ab3b
ShoxruxbekJaloldinov <jaloldinovshoxrux@gmail.com>
2023-09-22 08:56:43 +0300

    Add project files.

---
+++ DotNet Tutorial/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_Tutorial.Entities
+{
+    public class User
+    {
+        public long Id { get; set; }
+    }
+}
